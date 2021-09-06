9c0e7a73f0d01b448ae4b259f36ca044a64f003f
cczxl <1248041113@qq.com>
2021-09-06 09:48:51 +0800

    task:mp Autoconfig

--- src/main/java/com/example/demotest/config/AutoConfig.java
+++ src/main/java/com/example/demotest/config/AutoConfig.java
@@ -1,17 +1,23 @@
 package com.example.demotest.config;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.config.GlobalConfig;
 import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.po.TableFill;
 import com.baomidou.mybatisplus.generator.config.rules.DateType;
 
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
 
 @Configuration
 public class AutoConfig {
     public static void main(String[] args) {
-//       成绩代码生成器
+//       代码生成器
         AutoGenerator autoGenerator=new AutoGenerator();
 //       1 全局配置
         GlobalConfig gc = new GlobalConfig();
@@ -34,6 +40,30 @@ public class AutoConfig {
         packageConfig.setService("service");
         packageConfig.setParent("com.example.demotest");
         autoGenerator.setPackageInfo(packageConfig);
+
+
+        //4、策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        //strategy.setInclude("blog_tags","course","links","sys_settings","user_record"," user_say");
+                // 设置要映射的表名
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        strategy.setEntityLombokModel(true); // 自动lombok；
+        strategy.setLogicDeleteFieldName("deleted");
+        // 自动填充配置
+        TableFill gmtCreate = new TableFill("gmt_create", FieldFill.INSERT);
+        TableFill gmtModified = new TableFill("gmt_modified",
+                FieldFill.INSERT_UPDATE);
+        ArrayList<TableFill> tableFills = new ArrayList<>();
+        tableFills.add(gmtCreate);
+        tableFills.add(gmtModified);
+        strategy.setTableFillList(tableFills);
+                  // 乐观锁
+        strategy.setVersionFieldName("version");
+        strategy.setRestControllerStyle(true);
+        strategy.setControllerMappingHyphenStyle(true); //localhost:8080/hello_id_2
+        autoGenerator.setStrategy(strategy);
+
         autoGenerator.execute();
     }
 
